176c234b48ca4f9f418f7021bbce45f97600098b
Aleksandryi <skalovskyi.oleksandr.clg@chnu.edu.ua>
2023-07-21 09:57:27 +0300

    апгрейд валідації клієнта

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,6 @@ class Room < ApplicationRecord
   belongs_to :hotel
   has_many :room_photos, dependent: :destroy
 
+  validates :name, presence: true, uniqueness: true
   validates :area, presence: true, numericality: { greater_than: 0 }
 end
