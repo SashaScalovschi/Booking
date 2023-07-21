41502e6cc19f97d919add607f838bb0f7058996b
Aleksandryi <skalovskyi.oleksandr.clg@chnu.edu.ua>
2023-07-21 08:28:26 +0300

    апгрейд букінг контролер

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,13 +2,16 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @room = Room.find(params[:room_id])
   end
 
   def create
     @booking = Booking.new(booking_params)
-    if @booking.save
+
+    if room_available?(@booking.room, @booking.check_in_date, @booking.check_out_date) && @booking.save
       redirect_to @booking.room, notice: 'Booking was successfully created.'
     else
+      flash.now[:alert] = 'The room is not available for the selected dates.'
       render :new
     end
   end
@@ -17,10 +20,15 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
     @booking.destroy
 
-    redirect_to @booking.client, notice: 'Booking was successfully cancelled.'
+    redirect_to @booking.room, notice: 'Booking was successfully cancelled.'
   end
 
   private
+
+  def room_available?(room, check_in_date, check_out_date)
+    conflicting_bookings = room.bookings.where('(check_in_date BETWEEN ? AND ?) OR (check_out_date BETWEEN ? AND ?)', check_in_date, check_out_date, check_in_date, check_out_date)
+    conflicting_bookings.empty?
+  end
 
   def booking_params
     params.require(:booking).permit(:client_id, :room_id, :check_in_date, :check_out_date)
