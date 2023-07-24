d1cceafcc5442e25ec37527f7ac9eeaa9d3d73e4
Aleksandryi <skalovskyi.oleksandr.clg@chnu.edu.ua>
2023-07-24 17:14:14 +0300

    repo

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,5 +2,11 @@ class Client < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  # Optional: Add custom error messages
+  validates :name, presence: { message: "must be provided" }
+  validates :email, presence: { message: "must be provided" },
+            uniqueness: { message: "has already been taken" },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is invalid" }
 end
